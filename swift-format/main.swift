b0a6022747cfd527f9b01185bc0a374f4f54cb02
Dave Abrahams <dabrahams@apple.com>
2018-02-01 14:32:18 -0800

    Initial Commit

---
+++ swift-format/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swift-format
+//
+//  Created by Dave Abrahams on 2/1/18.
+//  Copyright © 2018 Dave Abrahams. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
