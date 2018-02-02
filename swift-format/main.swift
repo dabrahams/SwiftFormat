d09d5182960e0ff9430d3abad075d2a27b282dac
Dave Abrahams <dabrahams@apple.com>
2018-02-01 18:24:02 -0800

    Factor out SourceLoc from the rewriter

--- swift-format/main.swift
+++ swift-format/main.swift
@@ -9,75 +9,77 @@
 import Foundation
 import SwiftSyntax
 
+struct SourceLoc {
+    var line = 0
+    var column = 0
+
+    mutating func traverse(_ text: String) {
+        for c in text.utf16 {
+            switch c {
+            case 0xb, 0xc: line += 1// vertical linefeed, formfeed
+            case "\n".utf16.first: line += 1; column = 0
+            case "\r".utf16.first: column = 0
+            default: column += 1
+            }
+        }
+    }
+
+    mutating func traverse(_ nonToken: TriviaPiece) {
+        switch nonToken {
+        case .spaces(let n), .tabs(let n), .backticks(let n):
+            column += n
+
+        case .verticalTabs(let n), .formfeeds(let n):
+            line += n
+
+        case .newlines(let n), .carriageReturnLineFeeds(let n):
+            line += n
+            column = 0
+
+        case .carriageReturns(_):
+            column = 0
+
+        case .lineComment(let s),
+            .blockComment(let s),
+            .docLineComment(let s),
+            .docBlockComment(let s),
+            .garbageText(let s):
+
+            traverse(s)
+        }
+    }
+}
+
+extension SourceLoc : CustomStringConvertible {
+    var description: String {
+        return ":\(line):\(column)"
+    }
+}
+
+extension SourceLoc : CustomDebugStringConvertible {
+    var debugDescription: String {
+        return "SourceLoc(line: \(line): column: \(column))"
+    }
+}
+
 class Rewriter: SyntaxRewriter {
-  var tokens: [(String, line: Int, column: Int)] = []
-  var line = 0
-  var column = 0
+    var tokens: [(String, location: SourceLoc)] = []
+    var inputLocation = SourceLoc()
 
-  func updateSourceLocation(for text: String) {
-    for c in text.utf16 {
-      switch c {
-      case 0xb, 0xc: line += 1// vertical linefeed, formfeed
-      case "\n".utf16.first: line += 1; column = 0
-      case "\r".utf16.first: column = 0
-      default: column += 1
-      }
+    override func visit(_ token: TokenSyntax) -> Syntax {
+        for t in token.leadingTrivia {
+            inputLocation.traverse(t)
+        }
+
+        tokens.append((token.text, location: inputLocation))
+
+        inputLocation.traverse(token.text)
+        for t in token.trailingTrivia {
+            inputLocation.traverse(t)
+        }
+
+        return super.visit(token)
     }
-  }
-  
-  func updateSourceLocation(for nonToken: TriviaPiece) {
-    
-    switch nonToken {
-    /// A space ' ' character.
-    case .spaces(let n): column += n
-
-    /// A tab '\t' character.
-    case .tabs(let n): column += n
-
-    /// A vertical tab '\v' character.
-    case .verticalTabs(let n): line += n
-
-    /// A form-feed '\f' character.
-    case .formfeeds(let n): line += n
-
-    /// A newline '\n' character.
-    case .newlines(let n): line += n; column = 0
-
-    /// A carriage-return '\r' character.
-    case .carriageReturns(_): column = 0
-
-    /// A newline two bytes sequence consists of '\r' and '\n' characters.
-    case .carriageReturnLineFeeds(let n): line += n; column = 0
-
-    /// A backtick '`' character, used to escape identifiers.
-    case .backticks(let n): column += n
-
-    /// A developer line comment, starting with '//'
-    case .lineComment(let s): updateSourceLocation(for: s)
-
-    /// A developer block comment, starting with '/*' and ending with '*/'.
-    case .blockComment(let s): updateSourceLocation(for: s)
-
-    /// A documentation line comment, starting with '///'.
-    case .docLineComment(let s): updateSourceLocation(for: s)
-
-    /// A documentation block comment, starting with '/**' and ending with '*/.
-    case .docBlockComment(let s): updateSourceLocation(for: s)
-
-    /// Any skipped text.
-    case .garbageText(For: let s):
-      updateSourceLocation(for: s)
-    }
-  }
-    
-  override func visit(_ token: TokenSyntax) -> Syntax {
-    for t in token.leadingTrivia { updateSourceLocation(for: t) }    
-    tokens.append((token.text, line: line, column: column))
-    updateSourceLocation(for: token.text)
-    for t in token.trailingTrivia { updateSourceLocation(for: t) }
-    
-    return super.visit(token)
-  }
 }
 
 // Parse a .swift file
